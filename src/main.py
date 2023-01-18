8e6835984df49452cf8216bbfceb60b7a9ddcf15
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-01-18 20:53:52 +0200

    Finalize matrix calculations

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def generate_matrix(dimensions) -> list:
+  width, height = dimensions
+  
+
+
+  rng = np.random.default_rng()
+  
+  matrix = rng.random(size=[height, width], dtype=np.float32)
+  print(f"Matrix with shape {width}x{height} generated")
+
+  return matrix
+
+def main():
+  matrix_shapes = [[10**6, 10**3], [10**3, 10**6], [10**6, 1]]
+  #matrix_shapes = [[5,5], [5,5], [5,5]]
+  print("Generating matrixes...")
+  matrixes = list(map(generate_matrix, [(matrix_dimension[0], matrix_dimension[1]) for matrix_dimension in matrix_shapes]))
+
+  print("Multiplying matrix...") 
+  
+  matrix_a = matrixes[0]
+  matrix_b = matrixes[1]
+  matrix_c = matrixes[2]
+ 
+  multiplied_matrix = np.matmul(np.matmul(matrix_c, matrix_b), matrix_a)
+  
+  print("Final product shape: ", multiplied_matrix.shape)
+  print("Multiplied matrix: ", multiplied_matrix)
+
+main()
